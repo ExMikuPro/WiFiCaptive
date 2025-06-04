7b87b28ed2ac3777af2968fa0a2c40b6140b97df
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2025-06-04 15:16:49 +0800

    init: 初始化项目

---
+++ examples/ESPWiFiCaptiveEasyDemo/CustomSSID.ino
@@ -0,0 +1,15 @@
+/*
+  示例：CustomSSID
+  功能：指定自定义的热点名称（无密码），当设备无法连接 WiFi 时开启该 AP 并进入配网页面。
+*/
+
+#include <WiFiCaptive.h>
+WiFiCaptive captive;
+
+void setup() {
+  captive.begin("MyCaptiveAP");
+}
+
+void loop() {
+  captive.loop();
+}
